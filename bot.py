f915bfc44041544336c8403f01cd5a2b51774bd1
NourFahmy <35409519+NourFahmy@users.noreply.github.com>
2020-06-29 13:51:19 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,6 @@ def get_response(user_text):
 	if user_text.lower() in ['yes', 'yeah', 'ye', 'i loved it', 'i love it', 'i like it', 'i liked it',
 	'i loved them', 'i love them', 'i like them', 'i liked them', 'great job', 'good job']:
 		return 'Great to hear that! Stay tuned while I develop the ability to learn how to put forward more diverse films curated to you.'
-	if user_text.lower() in ['no','nope', 'i didn\'t', 'i didn\'t like it']
+	if user_text.lower() in ['no','nope', 'i didn\'t', 'i didn\'t like it']:
 		return 'Sorry to hear that. Please leave feedback & stay tuned while I develop the ability to learn how to put forward more diverse films curated to you.'
 	return 'Stay tuned while I develop the ability to talk to you.'
